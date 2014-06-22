13bc61cf323774795d4dc8c36a95abb31d2d6da3
Emma Kuo <emma.c.kuo@gmail.com>
2014-06-22 10:42:11 -0700

    fix repost/like markup

--- tpl/cite.php
+++ tpl/cite.php
@@ -4,10 +4,10 @@ foreach ($this->replyTo as $child) { ?>
     <a class="u-in-reply-to" href="<? echo $child->url ?>"></a>
 <? }
 foreach ($this->likeOf as $child) { ?>
-    <a class="u-in-reply-to" href="<? echo $child->url ?>"></a>
+    <a class="u-like-of" href="<? echo $child->url ?>"></a>
 <? }
 foreach ($this->repostOf as $child) { ?>
-    <a class="u-in-reply-to" href="<? echo $child->url ?>"></a>
+    <a class="u-repost-of" href="<? echo $child->url ?>"></a>
 <? } ?>
     <div class="blog-post">
 <? if ($this->isArticle()) { ?>
