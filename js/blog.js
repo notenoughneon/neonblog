8a75ea48857aa650ce7bb4ea9723c510403ad8a7
Emma Kuo <emma.c.kuo@gmail.com>
2014-07-06 11:14:18 -0700

    clean up javscript

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,8 @@
 $("time").each(function() {
-    var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-    var published = new Date($(this).attr("datetime"));
-    var delta = $.now() - published.getTime();
+    var month = ["Jan","Feb","Mar","Apr","May","Jun",
+                 "Jul","Aug","Sep","Oct","Nov","Dec"];
+    var dt = new Date($(this).attr("datetime"));
+    var delta = $.now() - dt.getTime();
     if (delta < 1000*60*60) {
         $(this).text(Math.floor(Math.max(0,delta/(1000*60))) + "m");
     } else if (delta < 1000*60*60*24) {
@@ -9,9 +10,10 @@ $("time").each(function() {
     } else if (delta < 1000*60*60*24*30) {
         $(this).text(Math.floor(delta/(1000*60*60*24)) + "d");
     } else if (delta < 1000*60*60*24*365) {
-        $(this).text(published.getDate() + " " + month[published.getMonth()]);
+        $(this).text(dt.getDate() + " " + month[dt.getMonth()]);
     } else {
-        $(this).text(published.getDate() + " " + month[published.getMonth()] + " " + published.getFullYear());
+        $(this).text(dt.getDate() + " " + month[dt.getMonth()]
+            + " " + dt.getFullYear());
     }
     return true;
 });
