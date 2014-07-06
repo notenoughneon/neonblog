98c1263a9176449bf2f9a567c6099510edccfbb1
Emma Kuo <emma.c.kuo@gmail.com>
2014-07-06 10:37:06 -0700

    fix timestamp formatting

--- js/blog.js
+++ js/blog.js
@@ -2,16 +2,13 @@ $("time").each(function() {
     var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     var published = new Date($(this).attr("datetime"));
     var delta = $.now() - published.getTime();
-    if (delta < 1000 * 60 * 60) {
-        var minutes = Math.ceil(delta/(1000*60));
-        $(this).text(minutes + "m");
-    } else if (delta < 1000 * 60 * 60 * 24) {
-        var hours = Math.ceil(delta/(1000*60*60));
-        $(this).text(hours + "h");
-    } else if (delta < 1000 * 60 * 60 * 24 * 30) {
-        var days = Math.ceil(delta/(1000*60*60*24));
-        $(this).text(days + "d");
-    } else if (delta < 1000 * 60 * 60 * 24 * 365) {
+    if (delta < 1000*60*60) {
+        $(this).text(Math.floor(Math.max(0,delta/(1000*60))) + "m");
+    } else if (delta < 1000*60*60*24) {
+        $(this).text(Math.floor(delta/(1000*60*60)) + "h");
+    } else if (delta < 1000*60*60*24*30) {
+        $(this).text(Math.floor(delta/(1000*60*60*24)) + "d");
+    } else if (delta < 1000*60*60*24*365) {
         $(this).text(published.getDate() + " " + month[published.getMonth()]);
     } else {
         $(this).text(published.getDate() + " " + month[published.getMonth()] + " " + published.getFullYear());
