ace1cedaadc5a5fe7fb9880f20c0ea79fb78fbe7
Emma Kuo <emma.c.kuo@gmail.com>
2014-07-12 12:03:37 -0700

    split regenerate/reindex scripts

--- scripts/reindex.php
+++ scripts/reindex.php
@@ -2,11 +2,7 @@
 require("lib/microformat.php");
 require("lib/common.php");
 
-
 $feed = new Microformat\Localfeed("postindex.json");
-foreach ($feed->getAll() as $post) {
-    echo "$post->file\n";
-    $post->save($config);
-}
+$feed->reload("#^p/.*\.html$#");
 
 ?>
